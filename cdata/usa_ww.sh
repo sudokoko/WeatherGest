acb75089ccf2508665f818399a3a16bc4682b757
koko <68549366+sudokoko@users.noreply.github.com>
2023-02-27 14:58:31 -0500

    Create usa_ww.sh

---
+++ cdata/usa_ww.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IMAGE_LOCATION_EXTERNAL="https://weather.cod.edu/cdata/satellite/regional/usa/current/usa.ww.gif"
+IMAGE_LOCATION_OUTPUT="~/ContentDelivery/met/img/"
+
+if [ -d $IMAGE_LOCATION_OUTPUT ]
+then
+  curl $IMAGE_LOCATION_EXTERNAL > $IMAGE_LOCATION_OUTPUT/overview.gif
+  curl -o "${IMAGE_LOCATION_OUTPUT}/overview/overview_${EPOCHSECONDS}.gif" $IMAGE_LOCATION_EXTERNAL
+else
+  echo "CRITICAL: Out directory does not exist."
+fi
